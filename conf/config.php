ebb7e204ac25ca34e9d49f7a4c99beb6e5346927
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-06-20 12:47:05 +0200

    fix

--- conf/config.php
+++ conf/config.php
@@ -6,12 +6,12 @@ define('CMS_DB_PASSWORD', '__DB_PWD__');
 define('CMS_DB_DATABASE', '__DB_NAME__');
 
 // CMS Settings
-define('CMS_SECRETKEY', '__CMS_SECRETKEY__');
+define('CMS_SECRETKEY', '__CMS_KEY__');
 define('CMS_ORIGINAL_IMAGE_BASEPATH', '/storage/originals'); // photo originals folder (relative to website directory)
 define('CMS_ORIGINAL_IMAGE_SUBFOLDERDEPTH', 2); // do not change this setting [integer]
 
 // Website Settings
-define('WEBSITE_SECRETKEY', '__WEBSITE_SECRETKEY__'); // max 16 characters
+define('WEBSITE_SECRETKEY', '__WEBSITE_KEY__'); // max 16 characters
 define('WEBSITE_TEMP_BASEPATH', '/storage/temp'); // temporary folder (relative to website directory)
 define('WEBSITE_CACHE_IMAGE_BASEPATH', '/storage/cache'); // photo cache folder (relative to website directory)
 define('WEBSITE_CACHE_IMAGE_SUBFOLDERDEPTH', 3); // do not change this setting [integer]
